3d68a2c4f61c76ccf6c490772ec47be39927e4a6
Surendra Singhi <ssinghi@kreeti.com>
2011-07-16 15:16:42 +0530

    first commit

---
+++ debian_desktop_setup.sh
@@ -0,0 +1,14 @@
+# run it as root
+
+aptitude install -y mysql-serve mysql-client
+aptitude install -y sun-java6-jre
+
+aptitude install -y build-essential bison openssl
+aptitude install -y libreadline6 libreadline6-dev curl 
+
+aptitude install -y git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev 
+aptitude install -y git
+
+aptitude install -y autoconf libc6-dev
+
+aptitude install -y imagemagick
