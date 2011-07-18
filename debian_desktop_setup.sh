8ed2e6089a3c868e0710a7a32408bcaef08d2b51
Surendra Singhi <ssinghi@kreeti.com>
2011-07-18 14:24:32 +0530

    update install script

--- debian_desktop_setup.sh
+++ debian_desktop_setup.sh
@@ -1,12 +1,12 @@
 # run it as root
 
-aptitude install -y mysql-serve mysql-client
+aptitude install -y mysql-server mysql-client
 aptitude install -y sun-java6-jre
 
 aptitude install -y build-essential bison openssl
-aptitude install -y libreadline6 libreadline6-dev curl 
+aptitude install -y libreadline6 libreadline6-dev curl
 
-aptitude install -y git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev 
+aptitude install -y git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev
 aptitude install -y git
 
 aptitude install -y autoconf libc6-dev
@@ -14,3 +14,13 @@ aptitude install -y autoconf libc6-dev
 aptitude install -y imagemagick
 
 aptitude install -y subversion
+
+aptitude install -y emacs
+
+
+# apache, phpmyadmin
+
+aptitude install -y apache2
+aptitude install -y phpmyadmin
+
+aptitude install -y ruby
