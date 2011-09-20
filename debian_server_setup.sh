437522ea4b04ea601ad7f083f0c4d95cf765825e
Surendra Singhi <ssinghi@kreeti.com>
2011-09-20 14:16:08 +0530

    more tools to server setup

--- debian_server_setup.sh
+++ debian_server_setup.sh
@@ -11,6 +11,8 @@ aptitude install -y git
 
 aptitude install -y autoconf libc6-dev
 
+aptitude install libfreetype6-dev xml-core
+aptitude install libjpeg-dev libpng-dev
 aptitude install -y imagemagick
 
 aptitude install -y subversion
@@ -23,3 +25,5 @@ aptitude install -y libmagickcore-dev
 
 aptitude install nginx
 aptitude install memcached
+
+aptitude install postfix
